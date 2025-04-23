6390e71adf286a9e58113f6a9133942967908b8d
Jyotiranjan Mishra <jyotiranjan0216@gmail.com>
2025-04-23 13:51:32 +0530

    New feature added

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GEM-ARC_FrontEnd/', // <-- update this to your actual repo name, e.g., 'trust-pay'
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1000 // Optional: increases chunk size warning limit to suppress that message
+    chunkSizeWarningLimit: 1000
   }
-})
+});
+
